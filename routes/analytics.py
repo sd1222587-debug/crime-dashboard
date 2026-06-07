9cec6d8964e952ff364aa0ce4ec2d8c3cc370c59
Susmita Dey <sd1222587@gmail.com>
2026-06-07 17:28:28 +0530

    update: connect to Railway MySQL database

--- routes/analytics.py
+++ routes/analytics.py
@@ -9,6 +9,7 @@ analytics_bp = Blueprint("analytics", __name__)
 def get_conn():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
         database=os.getenv("DB_NAME")
